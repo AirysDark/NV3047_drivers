c54909ddbdf78b17d2f1cfe280f9524d2de5ab45
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-09-23 02:49:47 +1000

    Integrate SD card driver into NV3047 hardware

--- src/NV3047.h
+++ src/NV3047.h
@@ -1,21 +1,27 @@
 #pragma once
+
 #include "Bus_Layers/SPI_Master.h"
 #include "Peripherals_HAL/TouchDriver.h"
 #include "Bus_Layers/RGB.h"
 #include "Peripherals_HAL/DisplayDriver.h"
+#include "Peripherals_HAL/SDCardDriver.h"
 #include "Core_Matrices/framebuffer.h"
 
 class NV3047 {
 private:
     SPI_Master spiMaster;
     TouchDriver touch;
+    SDCardDriver sdCard;
     RGB rgbBus;
     DisplayDriver display;
-    Framebuffer fb; // Centralised draw canvas management instance
+    Framebuffer fb;
 
 public:
     bool init();
+
     DisplayDriver& getDisplay();
     TouchDriver& getTouch();
-    Framebuffer& getCanvas(); // Exposes background buffer drawing actions directly
+    SDCardDriver& getSDCard();
+    const SDCardDriver& getSDCard() const;
+    Framebuffer& getCanvas();
 };
