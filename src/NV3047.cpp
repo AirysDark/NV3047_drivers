6378bda366ecc43ab00e63e1a42b8c23b5175112
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-09-23 02:49:52 +1000

    Auto-mount TF during V2.1 hardware initialization

--- src/NV3047.cpp
+++ src/NV3047.cpp
@@ -1,12 +1,22 @@
 #include "NV3047.h"
 
 bool NV3047::init() {
+    // Bring the shared SPI lines up first and immediately de-assert both
+    // GPIO0 TP_CS and GPIO10 SD_CS.
     if (!spiMaster.init()) return false;
     if (!touch.init(spiMaster.bus())) return false;
 
     if (!rgbBus.init()) return false;
     if (!display.init(rgbBus.getHandle())) return false;
     if (!fb.init(rgbBus.getHandle())) return false;
+
+    // V21_MATRIX_TEST represents the complete V2.1 peripheral profile.
+    // Try to mount TF automatically, but do not make LCD/touch startup depend
+    // on a card being physically inserted.
+    if (Config::SDCard::AUTO_MOUNT_WITH_ACTIVE_PROFILE &&
+        sdCard.isConfigured()) {
+        sdCard.init();
+    }
 
     return true;
 }
@@ -17,6 +27,14 @@ DisplayDriver& NV3047::getDisplay() {
 
 TouchDriver& NV3047::getTouch() {
     return touch;
+}
+
+SDCardDriver& NV3047::getSDCard() {
+    return sdCard;
+}
+
+const SDCardDriver& NV3047::getSDCard() const {
+    return sdCard;
 }
 
 Framebuffer& NV3047::getCanvas() {
