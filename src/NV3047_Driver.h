c4e7a24692df9609e9c2e17de7cf03d45523280e
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-09-23 15:10:34 +1000

    V3 inline high-level hot draw wrappers

--- src/NV3047_Driver.h
+++ src/NV3047_Driver.h
@@ -17,16 +17,75 @@ public:
     void sleep();
     void wake();
 
-    void pushPixels(int x, int y, int w, int h, const uint16_t* data);
-    void clear(uint16_t color = Config::COLOR_BLACK);
-    void fillScreen(uint16_t color);
-    bool present();
+    inline void pushPixels(
+        int x,
+        int y,
+        int w,
+        int h,
+        const uint16_t* data) {
 
-    void drawPixel(int16_t x, int16_t y, uint16_t color);
-    void fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color);
-    void drawHLine(int16_t x, int16_t y, int16_t w, uint16_t color);
-    void drawVLine(int16_t x, int16_t y, int16_t h, uint16_t color);
-    void drawRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color);
+        if (canvas) canvas->drawBitmap(x, y, w, h, data);
+    }
+
+    inline void clear(uint16_t color = Config::COLOR_BLACK) {
+        if (canvas) canvas->clear(color);
+    }
+
+    inline void fillScreen(uint16_t color) {
+        if (!canvas) return;
+        canvas->clear(color);
+        canvas->swap();
+    }
+
+    inline bool present() {
+        return canvas && canvas->swap();
+    }
+
+    inline void drawPixel(
+        int16_t x,
+        int16_t y,
+        uint16_t color) {
+
+        if (canvas) canvas->drawPixel(x, y, color);
+    }
+
+    inline void fillRect(
+        int16_t x,
+        int16_t y,
+        int16_t w,
+        int16_t h,
+        uint16_t color) {
+
+        if (canvas) canvas->fillRect(x, y, w, h, color);
+    }
+
+    inline void drawHLine(
+        int16_t x,
+        int16_t y,
+        int16_t w,
+        uint16_t color) {
+
+        if (canvas) canvas->drawHLine(x, y, w, color);
+    }
+
+    inline void drawVLine(
+        int16_t x,
+        int16_t y,
+        int16_t h,
+        uint16_t color) {
+
+        if (canvas) canvas->drawVLine(x, y, h, color);
+    }
+
+    inline void drawRect(
+        int16_t x,
+        int16_t y,
+        int16_t w,
+        int16_t h,
+        uint16_t color) {
+
+        if (canvas) canvas->drawRect(x, y, w, h, color);
+    }
 
     bool getTouch(uint16_t &x, uint16_t &y);
 
@@ -34,6 +93,6 @@ public:
     SDCardDriver* getSDCard();
     const SDCardDriver* getSDCard() const;
 
-    Framebuffer* getCanvas();
-    const Framebuffer* getCanvas() const;
+    inline Framebuffer* getCanvas() { return canvas; }
+    inline const Framebuffer* getCanvas() const { return canvas; }
 };
